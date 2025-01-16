e4d7dab0bc5b2195fd421741ce798f2dbe0d92d0
Hrasek1000cz <69074536+Hrasek1000cz@users.noreply.github.com>
2025-01-16 18:47:26 +0100

    0.8.0
    Přidány všechny mechaniky. Mělo by to být 100% funkční až na jeden bug, že ti to ne vždycky povolí po dohrání jednoho pole (velkého) si vybrat jiné velké pole. (zatím jsem nepřišel na to proč). Ale jinak by to snad mělo být plně hratelné

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     StartPlayerTurn();
     ChooseOffOn = true;
 });
-
+let bannedMtiles = [];
 //Funkce vykreslující herní plochu + vytvoření datové proměnné
 function drawBoard(){
     for(let x = 0; x < 9; x++){
@@ -37,34 +37,38 @@ function drawBoard(){
 //Funkce která bude zařizovat co se s políčkem stane po kliknutí na něj
 function revealTile(M_Tile, T_Tile){
     if(TurnOffOn){
-        if(choosenMTile === M_Tile){
-            let radek = 0;
-            radek = Math.floor(T_Tile / 3);
-            T_Tile -= radek * 3;
-            if(!DataArray[M_Tile][radek][T_Tile]){
-                DataArray[M_Tile][radek][T_Tile] = playerOnTurn + 1;
-                //Nahození znaku do .what
-                if(playerOnTurn){
-                    document.getElementById(`M${M_Tile +1}T${T_Tile + (radek * 3)}`).innerHTML = `<div class="textT">○</div>`;
-                }else{
-                    document.getElementById(`M${M_Tile +1}T${T_Tile + (radek * 3)}`).innerHTML = `<div class="textT">×</div>`;
+        if(typeof DataArray[M_Tile] === "object"){
+            if(choosenMTile === M_Tile){
+                let radek = 0;
+                radek = Math.floor(T_Tile / 3);
+                T_Tile -= radek * 3;
+                if(!DataArray[M_Tile][radek][T_Tile]){
+                    DataArray[M_Tile][radek][T_Tile] = playerOnTurn + 1;
+                    //Nahození znaku do .what
+                    if(playerOnTurn){
+                        document.getElementById(`M${M_Tile +1}T${T_Tile + (radek * 3)}`).innerHTML = `<div class="textT">○</div>`;
+                    }else{
+                        document.getElementById(`M${M_Tile +1}T${T_Tile + (radek * 3)}`).innerHTML = `<div class="textT">×</div>`;
+                    }
+                    setAroundColor(choosenMTile, "black");
+                    choosenMTile = T_Tile + (radek * 3);
+                    setAroundColor(choosenMTile, "red");
+                    controlM_Tile(M_Tile);
+                    StartPlayerTurn();
                 }
-                setAroundColor(choosenMTile, "black");
-                choosenMTile = T_Tile + (radek * 3);
-                setAroundColor(choosenMTile, "red");
-                controlM_Tile(M_Tile);
-                StartPlayerTurn();
             }
         }
     }
 }
 function choose(M_Tile){
-    if(ChooseOffOn){
-        M_Tile--;//M_Tile je v choose nastavena o 1 víc, takhle se nebude muset furt psát M_Tile - 1;
-        choosenMTile = M_Tile;
-        TurnOffOn = true;
-        ChooseOffOn = false;
-        setAroundColor(choosenMTile, "red");
+    if(!bannedMtiles.includes(M_Tile - 1)){
+        if(ChooseOffOn){
+            M_Tile--;//M_Tile je v choose nastavena o 1 víc, takhle se nebude muset furt psát M_Tile - 1;
+            choosenMTile = M_Tile;
+            TurnOffOn = true;
+            ChooseOffOn = false;
+            setAroundColor(choosenMTile, "red");
+        }
     }
 }
 //Měnič barvy MTile
@@ -89,22 +93,242 @@ function StartPlayerTurn(){
 
     //Kontrola zda v daném poli jde  ještě vůbec hýbat
     if(choosenMTile > -1){
-        let kontrolka = false;
-        for(let x = 0; x < 9; x++){
-            let radek = 0;
-            radek = Math.floor(x / 3);
-            let y = x - (radek * 3);
-            if(!DataArray[choosenMTile][radek][y]){
-                kontrolka = true;
+        if(bannedMtiles.includes(choosenMTile)){
+            setAroundColor(choosenMTile, "black");
+            TurnOffOn = false;
+            ChooseOffOn = true;
+            console.log("aaa");
+        }else{
+            let kontrolka = false;
+            for(let x = 0; x < 9; x++){
+                let radek = 0;
+                radek = Math.floor(x / 3);
+                let y = x - (radek * 3);
+                if(!DataArray[choosenMTile][radek][y]){
+                    kontrolka = true;
+                }
+            }
+            if(!kontrolka){
+                setAroundColor(choosenMTile, "black");
+                TurnOffOn = false;
+                ChooseOffOn = true;
+                DataArray[choosenMTile] = 0;
+                bannedMtiles.push(choosenMTile);
             }
         }
-        if(!kontrolka){
-            setAroundColor(choosenMTile, "black");
-            ChooseOffOn = true;
-        }
+    }else{
+        ChooseOffOn = false;
     }
+    
+    controlFinal();
 }
 //Kontrolovač jestli někdo nevyhrál v daném MTile;
 function controlM_Tile(M_Tile){
+    let kontrolka = false;
+    let winning = 0;
+    //Kontrola řádků
+    for(let x = 0; x < 3; x++){
+        let cislo = DataArray[M_Tile][x][0];
+        let counter = 0;
+        for(let y = 0; y < 3; y++){
+            if(DataArray[M_Tile][x][y] !== false){
+                if(DataArray[M_Tile][x][y] === cislo){
+                    counter++;
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+        if(counter === 3){
+            kontrolka = true;
+            winning = cislo;
+            break;
+        }
+    }
+    //Kontrola řádků
+    for(let x = 0; x < 3; x++){
+        let cislo = DataArray[M_Tile][0][x];
+        let counter = 0;
+        for(let y = 0; y < 3; y++){
+            if(DataArray[M_Tile][y][x] !== false){
+                if(DataArray[M_Tile][y][x] === cislo){
+                    counter++;
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+        if(counter === 3){
+            kontrolka = true;
+            winning = cislo;
+            break;
+        }
+    }
+    //Kontrola křížová 1
+    let cisloAAA = DataArray[M_Tile][0][0];
+    let counterAAA = 0;
+    for(let x = 0; x < 3; x++){
+        if(DataArray[M_Tile][x][x] !== false){
+            if(DataArray[M_Tile][x][x] === cisloAAA){
+                counterAAA++;
+            }else{
+                break;
+            }
+        }
+    }
+    if(counterAAA === 3){
+        kontrolka = true;
+        winning = cisloAAA;
+    }
+    //Kontrola křížová 2
+    let cisloBBB = DataArray[M_Tile][0][2];
+    let counterBBB = 0;
+    for(let x = 0; x < 3; x++){
+        if(DataArray[M_Tile][x][2 - x] !== false){
+            if(DataArray[M_Tile][x][2 - x] === cisloBBB){
+                counterBBB++;
+            }else{
+                break;
+            }
+        }
+    }
+    if(counterBBB === 3){
+        kontrolka = true;
+        winning = cisloBBB;
+    }
+
+    if(kontrolka){
+        DataArray[M_Tile] = winning;
+        console.log(DataArray[M_Tile]);
+        if(winning === 1){
+            document.getElementById(`M${M_Tile +1}`).innerHTML = `<div class="textM">×</div>`;
+        }else{
+            document.getElementById(`M${M_Tile +1}`).innerHTML = `<div class="textM">○</div>`;
+        }
+        bannedMtiles.push(M_Tile);
+    }
+}
+//Kontrolovač jestli někdo nevyhrál v daném MTile;
+function controlFinal(){
+    let doneArray = [];
+    let z = 0;
+    for(let x = 0; x < 3; x++){
+        doneArray.push([]);
+        for(let y = 0; y < 3; y++){
+            if(typeof DataArray[z] !== "object"){
+                doneArray[x].push(DataArray[z]);
+            }else{
+                doneArray[x].push(false);
+            }
+            z++;
+        }
+    }
+
+
+    //Kontrola řádků
+    for(let x = 0; x < 3; x++){
+        let cislo = doneArray[x][0];
+        let counter = 0;
+        for(let y = 0; y < 3; y++){
+            if(doneArray[x][y] !== false && doneArray[x][y] !== 0){
+                if(doneArray[x][y] === cislo){
+                    counter++;
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+        if(counter === 3){
+            kontrolka = true;
+            winning = cislo;
+            win(winning);
+            break;
+        }
+    }
+    //Kontrola řádků
+    for(let x = 0; x < 3; x++){
+        let cislo = doneArray[0][x];
+        let counter = 0;
+        for(let y = 0; y < 3; y++){
+            if(doneArray[y][x] !== false && doneArray[x][y] !== 0){
+                if(doneArray[y][x] === cislo){
+                    counter++;
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+        if(counter === 3){
+            kontrolka = true;
+            winning = cislo;
+            win(winning);
+            break;
+        }
+    }
+
+    //Kontrola křížová 1
+    let cisloAAA = doneArray[0][0];
+    let counterAAA = 0;
+    for(let x = 0; x < 3; x++){
+        if(doneArray[x][x] !== false){
+            if(doneArray[x][x] === cisloAAA){
+                counterAAA++;
+            }else{
+                break;
+            }
+        }
+    }
+    if(counterAAA === 3){
+        kontrolka = true;
+        winning = cisloAAA;
+        win(winning);
+    }
+    //Kontrola křížová 2
+    let cisloBBB = doneArray[0][2];
+    let counterBBB = 0;
+    for(let x = 0; x < 3; x++){
+        if(doneArray[x][2 - x] !== false){
+            if(doneArray[x][2 - x] === cisloBBB){
+                counterBBB++;
+            }else{
+                break;
+            }
+        }
+    }
+    if(counterBBB === 3){
+        kontrolka = true;
+        winning = cisloBBB;
+        win(winning);
+    }
+
+
+    //Kontrola ucpanosti
+    let counter2 = 0;
+    for(let x = 0; x < 3; x++){
+        if(!doneArray[x].includes(false)){
+            counter2++;
+        }
+    }
+    if(counter2 === 3){
+        alert("Nikdo nevyhrál")
+    }
+
+}
+function win(cislo){
+    ChooseOffOn = false;
+    TurnOffOn = false;
+    if(cislo === 1){
+        alert("Vyhrál ×");
+    }else{
+        alert("Vyhrál ○");
+    }
 
 }
